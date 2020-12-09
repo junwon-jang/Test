eaf86f79dcf7d677bf724df1ddab6b286d931040
wtd85 <wtd85@DESKTOP-B8D8KLD>
2020-12-09 21:53:29 +0900

    test

---
+++ src/main/java/asdf/asdf/asdf/TestMa.java
@@ -0,0 +1,7 @@
+package asdf.asdf.asdf;
+
+public class TestMa {
+	public static void main(String[] args) {
+		System.out.println("sdadasd");
+	}
+}
--- src/main/java/asdf/asdf/asdf/test.java
+++ src/main/java/asdf/asdf/asdf/test.java
@@ -4,6 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("asfewfaswef");
+		System.out.println("asdasdasd");
 	}
 
 }
